ec949d22ddfcf87a3d550dda76ab36620fdd726f
JIHYE KIM <dev.jiine@gmail.com>
2024-06-12 12:27:51 +0900

    배열을 이용한 리스트의 구현4

--- Chapter03/src/ArrayList.c
+++ Chapter03/src/ArrayList.c
@@ -39,3 +39,18 @@ int LNext(List * plist, LData * pdata)
   *pdata = plist->arr[plist->curPosition]; // pdata가 가리키는 공간에 데이터 저장
   return TRUE;
 }
+
+LData LRemove(List * plist)
+{
+  int rpos = plist->curPosition; // 삭제할 데이터의 인덱스 값 참조
+  int num = plist->numOfData;
+  int i;
+  LData rdata = plist->arr[rpos]; // 삭제할 데이터를 임시로 저장
+
+  // 삭제를 위한 데이터의 이동을 진행하는 반복문
+  for(i=rpos; i<num-1; i++)
+    plist->arr[i] = plist->arr[i+1];
+  (plist->numOfData)--; // 데이터의 수 감소
+  (plist->curPosition)--; // 참조위치를 하나 되돌림
+  return rdata; //삭제된 데이터의 반환
+}
