75bd7651d2597e4109efcf5f37b2db9ddcb34240
JIHYE KIM <dev.jiine@gmail.com>
2024-06-17 08:25:55 +0900

    단순 연결 리스트의 ADT와 구현3

---
+++ Chapter04/DLinkedRead.c
@@ -0,0 +1,78 @@
+// dummy 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node
+{
+  int data;
+  struct _node * next;
+} Node;
+
+int main(void)
+{
+  Node * head = NULL;
+  Node * tail = NULL;
+  Node * cur = NULL;
+
+  Node * newNode = NULL;
+  int readData;
+
+  head = (Node*)malloc(sizeof(Node)); // 더미노드 추가
+  tail = head;
+
+  //데이터 입력
+  while(1)
+  {
+    printf("자연수 입력: ");
+    scanf("%d", &readData);
+    if(readData < 1)
+      break;
+    
+    //노드의 추가 과정
+    newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = readData;
+    newNode->next = NULL;
+
+    tail->next = newNode;
+    tail = newNode;
+  }
+  printf("\n");
+  // 입력 받은 데이터의 출력 과정
+  printf("입력 받은 데이터의 전체출력! \n");
+  if(head == NULL)
+  {
+    printf("저장된 자연수가 존재하지 않습니다. \n");
+  }
+  else
+  {
+    cur = head;
+    while(cur->next != NULL) // 두 번째 이후의 데이터 출력
+    {
+      cur = cur->next;
+      printf("%d ", cur->data);
+    }
+  }
+  printf("\n\n");
+
+  // 메모리 해제 과정
+  if(head == NULL)
+  {
+    return 0; // 해제할 노드가 존재하지 않음
+  }
+  else
+  {
+    Node * delNode = head;
+    Node * delNextNode = head->next;
+    
+    while(delNextNode != NULL) // 두 번째 이후의 노드 삭제 위한 반복문
+    {
+      delNode = delNextNode;
+      delNextNode = delNextNode->next;
+      printf("%d를 삭제합니다. \n", delNode->data);
+      free(delNode); // 두 번째 이후의 노드 삭제
+    }
+  }
+  return 0;
+
+}
