db97c4f8fb98ae178a445faa0f30972a86ff1b96
JIHYE KIM <dev.jiine@gmail.com>
2024-07-11 08:28:39 +0900

    복잡하지만 효율적인 정렬 알고리즘 11

--- Chapter10/QuickSort.c
+++ Chapter10/QuickSort.c
@@ -12,14 +12,15 @@ int Partition(int arr[], int left, int right)
   int pivot = arr[left]; // 피벗의 위치는 가장 왼쪽 
   int low = left + 1;
   int high = right;
-  
-  while(low <= high) // 교차되지 않을 때까지 반복
+
+  // 교차되지 않을 때까지 반복. 항상 '참'일 수 밖에 없는 상황! 
+  while(low <= high) 
   {
-    // 피벗보다 큰 값을 찾는 과정
-    while(pivot > arr[low])
+    // 피벗보다 큰 값을 찾는 과정(경계 검사도 추가되어야함)
+    while(pivot >= arr[low] && low <= right) 
       low ++; // low를 오른쪽으로 이동
     // 피벗보다 작은 값을 찾는 과정
-    while(pivot < arr[high])
+    while(pivot <= arr[high] && high >= (left + 1)) 
       high--; // high를 왼쪽으로 이동
     // 교차되지 않은 상태라면 Swap 실행
     if(low <= high)
@@ -33,9 +34,9 @@ void QuickSort(int arr[], int left, int right)
 {
   if(left <= right)
   {
-    int pivot = Partition(arr, left, right);
-    QuickSort(arr, left, pivot-1);
-    QuickSort(arr, pivot + 1, right);
+    int pivot = Partition(arr, left, right); // 둘로 나눠서
+    QuickSort(arr, left, pivot-1); // 왼쪽 영역을 정렬
+    QuickSort(arr, pivot + 1, right); // 오른쪽 영역을 정렬
   }
 }
 
