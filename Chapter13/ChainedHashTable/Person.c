7c2e04081fb4f76b8482d0a2242aa0b2e6f80437
JiiihyeKim <zxc0585@hani.com>
2024-07-23 16:56:17 +0900

    코드 추가

---
+++ Chapter13/ChainedHashTable/Person.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Person.h"
+
+int GetSSN(Person * p)
+{
+  return p->ssn;
+}
+
+void ShowPerInfo(Person * p)
+{
+  printf(" %d \n", p->ssn);
+  printf(" %s \n", p->name);
+  printf(" %s \n\n", p->addr);
+}
+
+Person * MakePersonData(int ssn, char * name, char * addr)
+{
+  Person * newP = (Person*)malloc(sizeof(Person));
+
+  newP->ssn = ssn;
+  strcpy(newP->name, name);
+  strcpy(newP->addr, addr);
+  return newP;
+}
